bd41c835d166040fcefd308231e020c2a9ae9716
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-09-21 15:20:37 +0900

    ファイルの追加

---
+++ MayaCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class MayaCamera : MonoBehaviour {
+
+	public float dollySpeed;
+	public float tumbleSpeed;
+	public float trackSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
